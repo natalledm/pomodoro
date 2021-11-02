118c8717958dd8a8994d408c668ee4694ad495de
Natalle <natallemoura7@gmail.com>
2021-11-02 16:57:01 +0100

    Add pause/restart/continue buttons and js!

--- pomodoro.js
+++ pomodoro.js
@@ -1,33 +1,18 @@
 const btnStart = document.getElementById("pomodoro-start");
 const btnPause = document.getElementById("pomodoro-pause");
 const btnStop = document.getElementById("pomodoro-stop");
+const btnContinue = document.getElementById("pomodoro-continue");
+const btnRestart = document.getElementById("pomodoro-restart");
 
 
 const minutes = document.getElementById("pomodoro-minutes");
 const seconds = document.getElementById("pomodoro-seconds");
 
 
-// const timeNow = new Date().toLocaleTimeString(); //'5:37:27 PM'
-// const timeGoal = Date.parse(new Date().toLocaleDateString())
-// console.log(timeGoal)
-
-
-
-// function setTimeGoal(now){
-//   const time = now.split(':');
-//   let hour = parseInt(time[0]);
-//   let minutes = parseInt(time[1]);
-//   let seconds = parseInt(time[2]);
-//   console.log(hour, minutes, seconds)
-// }
-
-// setTimeGoal(timeNow)
-
-
-let mins = 25;
-let secs = 00;
-let started = false;
-let interv;
+let mins;
+let secs;
+let interval;
+let continueInterv;
 
 
 function countdown() {
@@ -56,23 +41,63 @@ function countdown() {
   seconds.innerText = secondsText;
 }
 
+function cleanInnerText() {
+  mins = 25;
+  secs = 0;
+  secondsText = '0' + secs;
+  minutes.innerText = mins;
+  seconds.innerText = secondsText;
+}
 
-
-function timer() {
-  interv = setInterval(countdown, 1000);
+function start() {
+  cleanInnerText();
+  interval = setInterval(countdown, 1000);
   btnStart.classList.add("hide");
   btnPause.classList.remove("hide");
   btnStop.classList.remove("hide");
+  btnRestart.classList.remove("hide");
 }
 
 function stop() {
-  clearInterval(interv);
+  clearInterval(interval);
+  clearInterval(continueInterv);
+  cleanInnerText();
   btnStart.classList.remove("hide");
+  btnRestart.classList.add("hide");
   btnPause.classList.add("hide");
   btnStop.classList.add("hide");
+  btnContinue.classList.add("hide");
 }
 
+function pause() {
+  clearInterval(interval);
+  btnContinue.classList.remove("hide");
+  btnRestart.classList.remove("hide");
+  btnPause.classList.add("hide");
+}
 
+function rmvPause() {
+  minutes.innerText = mins;
+  if(mins < 10) {
+    minutesText = '0' + mins;
+  }
+  seconds.innerText = secs;
+  if(secs < 10) {
+    secondsText = '0' + secs;
+    secondsText.innerText = secondsText;
+  }
+  continueInterv = setInterval(countdown, 1000);
+  btnContinue.classList.add("hide");
+  btnStart.classList.add("hide");
+}
 
-btnStart.addEventListener("click", timer);
+function restart() {
+  stop();
+  start();
+}
+
+btnStart.addEventListener("click", start);
+btnPause.addEventListener("click", pause);
 btnStop.addEventListener("click", stop);
+btnContinue.addEventListener("click", rmvPause);
+btnRestart.addEventListener("click", restart);
